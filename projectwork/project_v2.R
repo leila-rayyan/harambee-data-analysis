8aa6d0d1b5c89db75620fa6004d7bb03ae1a0379
Shiv <shivgunter@gmail.com>
2019-07-25 21:37:20 +0200

    leilas thing

--- projectwork/project_v2.R
+++ projectwork/project_v2.R
@@ -46,6 +46,8 @@ df5 = unique(df5)
 view(df5)
 data = merge(df5, datacft, by="unid")
 data = unique(data)
+
+
 ### Clean Data ###
 #Add Age data
 data <- data %>% 
@@ -58,36 +60,58 @@ data = select(data, -monthly_pay, -company_size, -job_start_date, -job_leave_dat
 #monthly pay
 #job leave and start date
 data2 = select(data, -unid, -dob, -survey_date_month, -peoplelive_15plus)
-view(data2)
 data3 = select(data2, -province)
-view(data3)
-
+#Take out survey_num column because it is 1 for all the data set. 
+d = select(data3, -survey_num)
 #Take out Na's
-d = na.omit(data3)
+d = na.omit(d)
+
 view(d)
 
 
-#Build Trainig Model
+### Visualizing data ###
+
+#historgram of the age of working people
+workingpeople  = filter(d, working==T)
+ggplot(workingpeople, aes(x = age)) + geom_histogram()
+mean(workingpeople$age)
+#histogram of the age of nonworking people
+nonworking = filter(d, working==F)
+ggplot(nonworking, aes(x = age,)) + geom_histogram()
+mean(nonworking$age)
+#shows that mean age for working and nonworking people is around the same. Therefore, age would probably be a bad predictor of working status. 
+
+
+
+### Build Trainig Model ###
 # y variable = working
 # x variables = everything else (for now)
+set.seed(17)
+train_index = sample(c(T, F), nrow(cars), prob = c(0.8, 0.2), replace = TRUE)
+d_train <- d[train_index,]
+d_test <- d[-train_index,]
+
 fit <- glm(working~ ., data2, family = binomial)
 summary(fit)
-#should the variables be factors or variables?
-### model output ###
-# sig factors:
-# survey_num (drop later)
-# province Gauteng (make sure provinces is a character)
-# province Mpumalanga
-# provinceNoth West
-# provinceNorther Cape
-# provinceWestern Cape
-# volunteerYes
-# leadershiproleYes
-# numchildren2
-# anygrantTrue
-# financial situtation_now3
-# financial_situation_nrow6
-# givemoney_yesTrue
+
+fit2 <- (titanic.rpart <- train(working ~ ., data = d, method = "rpart", trControl = TRAINCONTROL,
+                                metric = "Accuracy", na.action = na.omit, tuneGrid = expand.grid(cp = seq(0.000, 0.02, 0.0025))))
+
+fancyRpartPlot(fit2$finalModel, sub="Decision Tree Titanic")
+
+
+trControl <- trainControl(method = "cv", number = 10, verboseIter = TRUE)
+model_rpart <- train(working ~ ., data=d_train, method='rpart', trControl = trControl)
+model_rpart <- train(working ~ ., data=d_train, method='rpart', trControl = trControl, 
+                     tuneGrid = expand.grid(cp = seq(0.000, 0.02, 0.0025)))
+new_model <- train(working ~ ., data = d_train, method="ctree", trControl= trControl)
+#Compare the two models using resamples
+model_comp <- resamples(list(my_new_model = new_model, Rpart = model_rpart))
+summary(model_comp)
+new_model$results
+model_rpart$results
+
+
 
 
 # Test Model
